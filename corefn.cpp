d49565ca9509f582757f18b92e779554fcf75f43
Reinier <Reinierfs@users.noreply.github.com>
2016-04-19 11:32:06 +0200

    printf weggehaald
    Niet nodig

--- corefn.cpp
+++ corefn.cpp
@@ -8,24 +8,6 @@ extern int yyparse();
 extern NBlock* programBlock;
 
 
-llvm::Function* createPrintfFunction(CodeGenContext& context)
-{
-    std::vector<llvm::Type*> printf_arg_types;
-    printf_arg_types.push_back(llvm::Type::getInt8PtrTy(getGlobalContext())); //char*
-
-    llvm::FunctionType* printf_type =
-        llvm::FunctionType::get(
-            llvm::Type::getInt32Ty(getGlobalContext()), printf_arg_types, true);
-
-    llvm::Function *func = llvm::Function::Create(
-                printf_type, llvm::Function::ExternalLinkage,
-                llvm::Twine("printf"),
-                context.module
-           );
-    func->setCallingConv(llvm::CallingConv::C);
-    return func;
-}
-
 void createEchoFunction(CodeGenContext& context, llvm::Function* printfFn)
 {
     std::vector<llvm::Type*> echo_arg_types;
