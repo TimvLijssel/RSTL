ed790cd4d192ea85e294af90aac902bfe517df45
Tim van Lijssel <timvanlijssel1@gmail.com>
2016-04-20 16:41:22 +0200

    Wat tekst toegevoegd

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+printf "RSTL map wordt verwijderd...\n"
 rm -rf RSTL/
+printf "Klaar!\n\n"
+printf "Clonen van RSTL starten...\n"
 git clone https://github.com/TimvLijssel/RSTL.git
+printf "Klaar!\n\n"
+printf "Maken van RSTL starten...\n"
 cd RSTL/
 make
+printf "Klaar!\n\n\n"
+
+printf "Starten van example.txt in RSTL...\n"
 cat example.txt | ./parser
+printf "Klaar!\nProgramma eindigt\n"
